2a7d1e9323c09ced3c8b292f30070ef40288a502
ButWhyTho <normundsbrizs@gmail.com>
2017-11-15 01:27:11 +0200

    Update ServerSukodu.py

--- ServerSukodu.py
+++ ServerSukodu.py
@@ -1,56 +1,57 @@
 
-import random
-import time
-import os
-#import tkinter.tix
-import pickle
-from Tkinter import *
-#from Tkinter.constants import *
-#from Tkinter.tix import FileSelectBox, Tk
 import socket
 import threading
 
 
+#  To Send a random number between 1-9 to each Clients
 def Gen_send_sukodu_Random_9Numbers():
-    """
-    Algorithm:
-        Add a random number between 1-9 to each subgrid in the
-        board, do not add duplicate random numbers.
-    """
-    Data='912837645567894321321457698'
-    print "stringgggggggggggggggggggggggggggg", Data
-    send_all(str(Data))
-
-
-def check(rec):
-    if rec.isdigit() and int(rec) >0 :
-        connection.send(rec)
-    else:
-        print("Value not allowed, score deduced")
-        sys.exit()
-
-
-
+    Data='102232371415544665717837978'
+    print "The Random Generated Numbers have sent to the Cliens  ", Data
+    send_data(str(Data))
 
 connections = set()
-
-# Function to send processed data to the Clients
-def send_all(message):
+# To send processed data to the Clients
+def send_data(message):
     for connection in connections:
         print "Sending %s" % message
-        connection.send(message)
+        connection.send(str(message))
 
-# Function to receive data from clients
-def receive(connection):
+# To receive data from clients
+def receive_data(connection):
+    nicknameofplayers = ''
     while True:
         print "Waiting for message"
+
         message = connection.recv(1024)
-        if not message:
-            print "Closing connection and removing from registry"
-            connections.remove(connection)
-            return
-        print "Received %s, sending to all" % message
-        send_all(message)
+        if((len(message)>4 and len(message)<9)):
+            nicknameofplayers = message
+            nicknameslist(nicknameofplayers,address)
+        else:
+
+            if not message:
+                print "Closing connection and removing from registry"
+                connections.remove(connection)
+                return
+            print "Received %s, sending to all" % message, address
+        send_data(message)
+
+#Server Buffer to save the nicknames and address of clients
+list = []
+
+
+def nicknameslist(nick,address):
+    global list
+    list.append(nick)
+    list.append(address)
+    print "The Buffer of Server has a client ===>>>> :",list
+    print "Clients are are now connected....  ===>>> :",list
+
+
+
+
+
+
+
 
 
 
@@ -59,16 +60,12 @@ sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sckt.bind(('127.0.0.1', 9999))
 sckt.listen(10)
 
-# We must accept connections in a loop
+# The connections should be in a loop
 while True:
     print "Waiting for a connection"
-
     (connection, address) = sckt.accept()
-
     print "Connection received. Adding to registry"
+    print "The Client", address, "is now connected..."
     connections.add(connection)
-
-    print "Spawning receiver"
-    threading.Thread(target = receive, args=[connection]).start()
-   # print "77777777777777777777", threading.current_thread()
+    threading.Thread(target = receive_data, args=[connection]).start()
     Gen_send_sukodu_Random_9Numbers()
